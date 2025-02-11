a348dad2b8f3e07affeb4c7c040e8fedf30768a7
Darko <darko.spasojevic33@gmail.com>
2025-02-11 21:49:06 +0100

    media root menjam...

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -187,9 +187,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 

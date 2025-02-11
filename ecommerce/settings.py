8d5b7c55a5f399c6c188f1067991966252583c49
Darko <darko.spasojevic33@gmail.com>
2025-02-11 22:56:35 +0100

    promenio si static root sada je samo media a ne static/media

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -188,7 +188,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
